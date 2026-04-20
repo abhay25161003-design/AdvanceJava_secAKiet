35134a89ac091ad509c6c4a779c71b177d4d6013
Abhay Tyagi <abhay.25161003@kiet.edu>
2026-04-20 18:53:39 +0530

    Add files via upload

---
+++ FilterFive.java
@@ -0,0 +1,20 @@
+package calc;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FilterFive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 List<String> data=Arrays.asList("Java", null, "Spring", null, "React");
+	        ArrayList<String> list =new ArrayList<String>();
+	        list.addAll(data);
+	       List<String> notNullList= list.stream()
+	        .filter(lang->lang!=null)
+	        .toList();
+	       System.out.println(notNullList);
+	}
+
+}
